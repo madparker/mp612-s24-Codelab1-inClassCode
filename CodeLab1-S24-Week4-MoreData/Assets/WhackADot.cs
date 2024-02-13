53b2f6146c814246fb2c03ec987a2a487b6c8e5d
Matthew Parker <maddparker@gmail.com>
2024-02-13 14:31:25 -0500

    Init Week 4 upload
    Added a dot and an init script

---
+++ CodeLab1-S24-Week4-MoreData/Assets/WhackADot.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhackADot : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("You whacked a Dot!!!");
+    }
+}

5c33fc3f00eb3e0d49d4be0d812b916e2019dd7b
Matthew Parker <maddparker@gmail.com>
2024-02-13 16:43:36 -0500

    Added highscores
    persisting to file and parsed and read in

--- CodeLab1-S24-Week4-MoreData/Assets/Scripts/GameManager.cs
+++ CodeLab1-S24-Week4-MoreData/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,7 +13,11 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI display;
     
     public int score;
-
+    
+    const string FILE_DIR = "/DATA/";
+    const string DATA_FILE = "highScores.txt";
+    string FILE_FULL_PATH;
+    
     public int Score
     {
         get
@@ -26,11 +31,37 @@ public class GameManager : MonoBehaviour
 
             if (isHighScore(score))
             {
+                int highScoreSlot = -1;
+
+                for (int i = 0; i < HighScores.Count; i++)
+                {
+                    if (score > highScores[i])
+                    {
+                        highScoreSlot = i;
+                        break;
+                    }
+                }
                 
+                highScores.Insert(highScoreSlot, score);
+
+                highScores = highScores.GetRange(0, 5);
+
+                string scoreBoardText = "";
+
+                foreach (var highScore in highScores)
+                {
+                    scoreBoardText += highScore + "\n";
+                }
+
+                highScoresString = scoreBoardText;
+                
+                File.WriteAllText(FILE_FULL_PATH, highScoresString);
             }
         }
 
     }
+
+    string highScoresString = "";
     
     List<int> highScores;
 
@@ -41,11 +72,18 @@ public class GameManager : MonoBehaviour
             if (highScores == null)
             {
                 highScores = new List<int>();
+
+                highScoresString = File.ReadAllText(FILE_FULL_PATH);
+
+                highScoresString = highScoresString.Trim();
                 
-                highScores.Add(0);
-                highScores.Insert(0, 3);
-                highScores.Insert(1, 2);
-                highScores.Insert(2, 1);
+                string[] highScoreArray = highScoresString.Split("\n");
+
+                for (int i = 0; i < highScoreArray.Length; i++)
+                {
+                    int currentScore = Int32.Parse(highScoreArray[i]);
+                    highScores.Add(currentScore);
+                }
             }
 
             return highScores;
@@ -74,7 +112,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        FILE_FULL_PATH = Application.dataPath + FILE_DIR + DATA_FILE;
     }
 
     // Update is called once per frame
@@ -86,7 +124,8 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            display.text = "GAME OVER\nFINAL SCORE: " + score;
+            display.text = "GAME OVER\nFINAL SCORE: " + score +
+                           "\nHigh Scores:\n" + highScoresString;
         }
 
         //add the fraction of a second between frames to timer

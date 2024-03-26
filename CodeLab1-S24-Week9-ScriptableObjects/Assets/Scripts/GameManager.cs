4f952f4123288f0531e008db297d0dda19d555be
Matthew Parker <maddparker@gmail.com>
2024-03-26 16:46:58 -0400

    Added functional buttons in all directions

--- CodeLab1-S24-Week9-ScriptableObjects/Assets/Scripts/GameManager.cs
+++ CodeLab1-S24-Week9-ScriptableObjects/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI descriptionUI;
 
     public LocationScriptableObject currentLocation;
+
+    public Button buttonNorth;
+    public Button buttonSouth;
+    public Button buttonWest;
+    public Button buttonEast;
     
     // Start is called before the first frame update
     void Start()
@@ -21,5 +27,29 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MoveDir(string dirChar)
+    {
+        switch (dirChar)
+        {
+           case "N":
+               currentLocation = currentLocation.north;
+               break;
+           case "S":
+               currentLocation = currentLocation.south;
+               break;
+           case "W":
+               currentLocation = currentLocation.west;
+               break;
+           case "E":
+               currentLocation = currentLocation.east;
+               break;
+          default:
+              Debug.Log("WTF? That's a valid dir");
+              break;
+        }
+        
+        currentLocation.UpdateCurrentLocation(this);
     }
 }
--- CodeLab1-S24-Week9-ScriptableObjects/Assets/Scripts/LocationScriptableObject.cs
+++ CodeLab1-S24-Week9-ScriptableObjects/Assets/Scripts/LocationScriptableObject.cs
@@ -15,6 +15,9 @@ public class LocationScriptableObject : ScriptableObject
     public string locationDesc;
 
     public LocationScriptableObject north;
+    public LocationScriptableObject south;
+    public LocationScriptableObject west;
+    public LocationScriptableObject east;
     
     public void PrintLocation()
     {
@@ -29,5 +32,45 @@ public class LocationScriptableObject : ScriptableObject
     {
         gm.titleUI.text = locationName;
         gm.descriptionUI.text = locationDesc;
+
+        if (north == null)
+        {
+            gm.buttonNorth.gameObject.SetActive(false);
+        }
+        else
+        {
+            gm.buttonNorth.gameObject.SetActive(true);
+            north.south = this;
+        }
+
+        if (south == null)
+        {
+            gm.buttonSouth.gameObject.SetActive(false);
+        }
+        else
+        {
+            gm.buttonSouth.gameObject.SetActive(true);
+            south.north = this;
+        }
+
+        if (east == null)
+        {
+            gm.buttonEast.gameObject.SetActive(false);
+        }
+        else
+        {
+            gm.buttonEast.gameObject.SetActive(true);
+            east.west = this;
+        }
+
+        if (west == null)
+        {
+            gm.buttonWest.gameObject.SetActive(false);
+        }
+        else
+        {
+            gm.buttonWest.gameObject.SetActive(true);
+            west.east = this;
+        }
     }
 }
